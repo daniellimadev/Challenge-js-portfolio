7991e159d9a57612617f5e6e044ef8688cdecd9f
daniellimadev <pdaniel643@gmail.com>
2024-05-13 12:51:32 -0300

    challenge finished

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/daniellimadev/Challenge-js-portfolio/main/data/profile.json?token=GHSAT0AAAAAACRXIX24LQGCNXLQ4QGBBFMOZSCF2HA';
+    const url = 'https://raw.githubusercontent.com/daniellimadev/Challenge-js-portfolio/main/data/profile.json?token=GHSAT0AAAAAACRXIX24JGIOCRD32VLYATOAZSCG2LQ';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
